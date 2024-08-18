ff944f3389c7635b502e34a5350f9d5f9a0c9630
EliDev <oscarmateoelias@gmail.com>
2024-08-18 13:19:06 -0600

    Add ExpenseList component for displaying expenses

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm";
 import BudgetTracker from "./components/BudgetTracker";
 import { useBudget } from "./hooks/useBudget";
 import ExpenseModal from "./components/ExpenseModal";
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
   const { state } = useBudget();
@@ -22,8 +23,7 @@ function App() {
 
       {isValidBudget && (
         <main className="max-w-3xl mx-auto py-10">
-
-          
+          <ExpenseList />
 
           <ExpenseModal />
         </main>
